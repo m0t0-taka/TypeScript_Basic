88b8569da0b69ffcd60adc83fa733a094c27bcd9
m0t0-taka <81753585+m0t0-taka@users.noreply.github.com>
2021-12-11 19:52:57 +0900

    返却値の型指定

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { Practice1 } from "./practices/Practice1";
+import { Practice２ } from "./practices/Practice２";
 import "./styles.css";
 
 export default function App() {
   return (
     <div className="App">
       <Practice1 />
+      <Practice２ />
     </div>
   );
 }
