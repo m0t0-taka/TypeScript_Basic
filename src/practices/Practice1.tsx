293326e51522ac8c7876f0237a0401f36d15f445
m0t0-taka <81753585+m0t0-taka@users.noreply.github.com>
2021-12-11 18:30:04 +0900

    Initial commit

---
+++ src/practices/Practice1.tsx
@@ -0,0 +1,7 @@
+export const Practice1 = () => {
+  return (
+    <div>
+      <p>練習問題：引数の型指定</p>
+    </div>
+  );
+};
---
+++ src/typeList.ts
@@ -0,0 +1,12 @@
+/* eslint-disable @typescript-eslint/no-unused-vars*/
+
+let bool: boolean = true;
+let num: number = 0;
+let str: string = "ABC";
+
+let arr1: Array<number> = [0, 1, 2];
+let arr2: number[] = [0, 1, 2];
+
+let any1: any = false;
+
+let null1: null = null;
